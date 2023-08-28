04867a2014604134682fa6c560a3832b6ea43ad1
Klaw807 <951466487@qq.com>
2023-08-28 13:01:35 +0800

    add tpcc modify for mysql 5.6

--- src/main/resources/benchmarks/tpcc/ddl-mysql.sql
+++ src/main/resources/benchmarks/tpcc/ddl-mysql.sql
@@ -51,8 +51,9 @@ CREATE TABLE stock (
     s_dist_08    char(24)      NOT NULL,
     s_dist_09    char(24)      NOT NULL,
     s_dist_10    char(24)      NOT NULL,
-    FOREIGN KEY (s_w_id) REFERENCES warehouse (w_id) ON DELETE CASCADE,
-    FOREIGN KEY (s_i_id) REFERENCES item (i_id) ON DELETE CASCADE,
+    -- yyx comment for mysql 5.6
+    -- FOREIGN KEY (s_w_id) REFERENCES warehouse (w_id) ON DELETE CASCADE,
+    -- FOREIGN KEY (s_i_id) REFERENCES item (i_id) ON DELETE CASCADE,
     PRIMARY KEY (s_w_id, s_i_id)
 );
 
@@ -68,7 +69,8 @@ CREATE TABLE district (
     d_city      varchar(20)    NOT NULL,
     d_state     char(2)        NOT NULL,
     d_zip       char(9)        NOT NULL,
-    FOREIGN KEY (d_w_id) REFERENCES warehouse (w_id) ON DELETE CASCADE,
+    -- yyx comment for mysql 5.6
+    -- FOREIGN KEY (d_w_id) REFERENCES warehouse (w_id) ON DELETE CASCADE,
     PRIMARY KEY (d_w_id, d_id)
 );
 
@@ -94,7 +96,8 @@ CREATE TABLE customer (
     c_since        timestamp      NOT NULL DEFAULT CURRENT_TIMESTAMP,
     c_middle       char(2)        NOT NULL,
     c_data         varchar(500)   NOT NULL,
-    FOREIGN KEY (c_w_id, c_d_id) REFERENCES district (d_w_id, d_id) ON DELETE CASCADE,
+    -- yyx comment for mysql 5.6
+    -- FOREIGN KEY (c_w_id, c_d_id) REFERENCES district (d_w_id, d_id) ON DELETE CASCADE,
     PRIMARY KEY (c_w_id, c_d_id, c_id)
 );
 
@@ -106,9 +109,11 @@ CREATE TABLE history (
     h_w_id   int           NOT NULL,
     h_date   timestamp     NOT NULL DEFAULT CURRENT_TIMESTAMP,
     h_amount decimal(6, 2) NOT NULL,
-    h_data   varchar(24)   NOT NULL,
-    FOREIGN KEY (h_c_w_id, h_c_d_id, h_c_id) REFERENCES customer (c_w_id, c_d_id, c_id) ON DELETE CASCADE,
-    FOREIGN KEY (h_w_id, h_d_id) REFERENCES district (d_w_id, d_id) ON DELETE CASCADE
+    h_data   varchar(24)   NOT NULL
+    -- yyx comment for mysql 5.6
+    -- ,
+    -- FOREIGN KEY (h_c_w_id, h_c_d_id, h_c_id) REFERENCES customer (c_w_id, c_d_id, c_id) ON DELETE CASCADE,
+    -- FOREIGN KEY (h_w_id, h_d_id) REFERENCES district (d_w_id, d_id) ON DELETE CASCADE
 );
 
 CREATE TABLE oorder (
@@ -121,7 +126,8 @@ CREATE TABLE oorder (
     o_all_local  int       NOT NULL,
     o_entry_d    timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (o_w_id, o_d_id, o_id),
-    FOREIGN KEY (o_w_id, o_d_id, o_c_id) REFERENCES customer (c_w_id, c_d_id, c_id) ON DELETE CASCADE,
+    -- yyx comment for mysql 5.6
+    -- FOREIGN KEY (o_w_id, o_d_id, o_c_id) REFERENCES customer (c_w_id, c_d_id, c_id) ON DELETE CASCADE,
     UNIQUE (o_w_id, o_d_id, o_c_id, o_id)
 );
 
@@ -129,7 +135,8 @@ CREATE TABLE new_order (
     no_w_id int NOT NULL,
     no_d_id int NOT NULL,
     no_o_id int NOT NULL,
-    FOREIGN KEY (no_w_id, no_d_id, no_o_id) REFERENCES oorder (o_w_id, o_d_id, o_id) ON DELETE CASCADE,
+    -- yyx comment for mysql 5.6
+    -- FOREIGN KEY (no_w_id, no_d_id, no_o_id) REFERENCES oorder (o_w_id, o_d_id, o_id) ON DELETE CASCADE,
     PRIMARY KEY (no_w_id, no_d_id, no_o_id)
 );
 
@@ -144,8 +151,9 @@ CREATE TABLE order_line (
     ol_supply_w_id int           NOT NULL,
     ol_quantity    int           NOT NULL,
     ol_dist_info   char(24)      NOT NULL,
-    FOREIGN KEY (ol_w_id, ol_d_id, ol_o_id) REFERENCES oorder (o_w_id, o_d_id, o_id) ON DELETE CASCADE,
-    FOREIGN KEY (ol_supply_w_id, ol_i_id) REFERENCES stock (s_w_id, s_i_id) ON DELETE CASCADE,
+    -- yyx comment for mysql 5.6
+    -- FOREIGN KEY (ol_w_id, ol_d_id, ol_o_id) REFERENCES oorder (o_w_id, o_d_id, o_id) ON DELETE CASCADE,
+    -- FOREIGN KEY (ol_supply_w_id, ol_i_id) REFERENCES stock (s_w_id, s_i_id) ON DELETE CASCADE,
     PRIMARY KEY (ol_w_id, ol_d_id, ol_o_id, ol_number)
 );
 
